0e4f569f8a49087e039e18464b42b3b2ef7bd45e
CaoMeiYouRen <996881204@qq.com>
2024-10-07 22:05:48 +0800

    refactor: 重构 错误捕获逻辑

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { timeout } from 'hono/timeout'
-import { HTTPException } from 'hono/http-exception'
-import { StatusCode } from 'hono/utils/http-status'
 import { cors } from 'hono/cors'
 import { secureHeaders } from 'hono/secure-headers'
-import { TIMEOUT } from './env'
+import { showRoutes } from 'hono/dev'
+import { __DEV__, TIMEOUT } from './env'
 import { winstonLogger } from './utils/logger'
+import { errorhandler, notFoundHandler } from './middlewares/error'
 
 const app = new Hono()
 
@@ -17,28 +17,15 @@ app.use(timeout(TIMEOUT))
 app.use(cors())
 app.use(secureHeaders())
 
-app.onError((error, c) => {
-    const message = process.env.NODE_ENV === 'production' ? `${error.name}: ${error.message}` : error.stack
-    let status = 500
-    let statusText = 'INTERNAL_SERVER_ERROR'
-    if (error instanceof HTTPException) {
-        const response = error.getResponse()
-        status = response.status
-        statusText = response.statusText
-    }
-    c.status(status as StatusCode)
-    const method = c.req.method
-    const requestPath = c.req.path
-    winstonLogger.error(`Error in ${method} ${requestPath}: \n${message}`)
-    return c.json({
-        status,
-        statusText,
-        message,
-    })
-})
+app.onError(errorhandler)
+app.notFound(notFoundHandler)
 
 app.all('/', (c) => c.json({
     message: 'Hello Hono!',
 }))
+
+__DEV__ && showRoutes(app, {
+    verbose: true,
+})
 
 export default app
