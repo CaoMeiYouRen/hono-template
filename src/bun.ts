1ec47386f7557c5e4a7444c1a36210f6c1b52145
CaoMeiYouRen <996881204@qq.com>
2024-12-11 21:18:23 +0800

    feat(bun): 添加 bun 运行时静态文件服务；更新 bun 开发命令以支持文件监听 - 在 bun 运行时中添加静态文件服务，支持访问 public 目录下的文件 - 修改 dev:bun 命令，添加 --watch 参数以支持文件更改时自动重启

--- src/bun.ts
+++ src/bun.ts
@@ -1,7 +1,11 @@
+import { serveStatic } from 'hono/bun'
 import { name } from '../package.json'
 import app from './app'
 import { PORT } from './env'
 import logger from './middlewares/logger'
+
+// bun 运行时添加静态文件服务。
+app.get('/*', serveStatic({ root: './public' }))
 
 logger.info(`${name} 启动成功，访问地址：http://localhost:${PORT}`)
 
