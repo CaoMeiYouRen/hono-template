4f26c766105a333b1e48dbc4b5cbe4a7d0affe8a
CaoMeiYouRen <996881204@qq.com>
2024-12-19 16:35:13 +0800

    feat(ci): 添加 GitHub Actions 工作流条件；添加 GitHub PR 评论功能 - 只在源项目中运行 benchmarks 和 build 任务 - 添加 Bun 类型定义和依赖 - 添加 @types/bun 依赖 - 添加 better-bytes 依赖 - 在 benchmarks 脚本中添加 GitHub PR 评论功能 - 使用 Octokit 创建评论并包含系统信息和 benchmarks 结果

--- scripts/benchmarks.ts
+++ scripts/benchmarks.ts
@@ -2,6 +2,8 @@ import os from 'os'
 import path from 'path'
 import autocannon, { Client, Options } from 'autocannon'
 import dotenv from 'dotenv'
+import { Octokit } from 'octokit'
+import * as betterBytes from 'better-bytes'
 
 dotenv.config({
     path: [
@@ -18,6 +20,10 @@ const PORT = Number(process.env.PORT || 3000)
 
 const BASE_URL = `http://localhost:${PORT}`
 
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN || ''
+const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY || ''
+const GITHUB_PR_NUMBER = process.env.GITHUB_PR_NUMBER || ''
+
 async function runAutocannon(param: Options) {
     const result = await autocannon({
         connections: cpuCount * 2, // 并发连接数
@@ -28,7 +34,22 @@ async function runAutocannon(param: Options) {
         // bailout: 1000,
         ...param,
     })
-    console.log(`url: ${param.url}\n${autocannon.printResult(result)}`)
+    const resultOutput = `url: ${param.url}\n${autocannon.printResult(result)}`
+    console.log(resultOutput)
+    if (GITHUB_TOKEN && GITHUB_REPOSITORY && GITHUB_PR_NUMBER) { // 如果有 GITHUB_PR_NUMBER，则创建评论
+        const octokit = new Octokit({
+            auth: GITHUB_TOKEN,
+        })
+        const runtime = typeof Bun === 'undefined' ? 'Node.js' : 'Bun'
+        const version = typeof Bun === 'undefined' ? process.versions.node : Bun.version
+        const osInfo = `System: ${os.type()} ${os.release()} (${os.arch()})<br>${runtime}: ${version}<br>CPU: ${os.cpus().length} cores<br>Memory: ${betterBytes.format(os.totalmem())} MB`
+        await octokit.rest.issues.createComment({
+            owner: GITHUB_REPOSITORY.split('/')[0],
+            repo: GITHUB_REPOSITORY.split('/')[1],
+            issue_number: parseInt(GITHUB_PR_NUMBER),
+            body: `## System Info \n\n${osInfo}\n\n## Benchmarks Results\n\n\`\`\`\n${resultOutput}\n\`\`\``,
+        })
+    }
     return result
 }
 
