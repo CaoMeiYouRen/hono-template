562148766eb7db61903d7e64d2a03613d0d5fc13
CaoMeiYouRen <996881204@qq.com>
2024-12-21 02:24:40 +0800

    ci(benchmarks): 添加系统信息和基准测试结果输出 - 在基准测试中输出系统信息和运行时版本 - 优化 GitHub PR 评论内容格式

--- scripts/benchmarks.ts
+++ scripts/benchmarks.ts
@@ -24,10 +24,15 @@ const GITHUB_TOKEN = process.env.GITHUB_TOKEN || ''
 const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY || ''
 const GITHUB_PR_NUMBER = process.env.GITHUB_PR_NUMBER || ''
 
+const runtime = typeof Bun === 'undefined' ? 'Node.js' : 'Bun'
+const version = typeof Bun === 'undefined' ? process.versions.node : Bun.version
+const osInfo = `System: ${os.type()} ${os.release()} (${os.arch()})<br>${runtime}: ${version}<br>CPU: ${os.cpus().length} cores<br>Memory: ${betterBytes.format(os.totalmem())}`
+
 async function runAutocannon(param: Options) {
+    console.log(`正在测试 url: ${param.url}`)
     const result = await autocannon({
         connections: cpuCount * 2, // 并发连接数
-        // workers: cpuCount,
+        workers: cpuCount,
         duration: 10, // 测试持续时间（秒）
         timeout: 3, // 超时时间 (秒)
         // overallRate: 100, // 设置限流器的速率，单位为每秒请求数
@@ -35,19 +40,18 @@ async function runAutocannon(param: Options) {
         ...param,
     })
     const resultOutput = `url: ${param.url}\n${autocannon.printResult(result)}`
-    console.log(resultOutput)
+
+    const body = `## System Info \n\n${osInfo}\n\n## Benchmarks Results\n\n\`\`\`\n${resultOutput}\n\`\`\``
+    console.log(`${osInfo.replaceAll('<br>', '\n')}\n${resultOutput}`)
     if (GITHUB_TOKEN && GITHUB_REPOSITORY && GITHUB_PR_NUMBER) { // 如果有 GITHUB_PR_NUMBER，则创建评论
         const octokit = new Octokit({
             auth: GITHUB_TOKEN,
         })
-        const runtime = typeof Bun === 'undefined' ? 'Node.js' : 'Bun'
-        const version = typeof Bun === 'undefined' ? process.versions.node : Bun.version
-        const osInfo = `System: ${os.type()} ${os.release()} (${os.arch()})<br>${runtime}: ${version}<br>CPU: ${os.cpus().length} cores<br>Memory: ${betterBytes.format(os.totalmem())}`
         await octokit.rest.issues.createComment({
             owner: GITHUB_REPOSITORY.split('/')[0],
             repo: GITHUB_REPOSITORY.split('/')[1],
             issue_number: parseInt(GITHUB_PR_NUMBER),
-            body: `## System Info \n\n${osInfo}\n\n## Benchmarks Results\n\n\`\`\`\n${resultOutput}\n\`\`\``,
+            body,
         })
     }
     return result
